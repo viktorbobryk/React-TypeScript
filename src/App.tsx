37ccb149898b910a8bd2d7d2eee79c88c99c88eb
viktor <viktor.bobryk@gmail.com>
2021-01-24 23:16:39 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import {Navbar} from './components/Navbar';
 import {TodosPage} from'./pages/TodosPage'
 import {AboutPage} from "./pages/AboutPage";
 
 const App: React.FC = () => {
-
   return (
     <BrowserRouter>
       <Navbar/>
